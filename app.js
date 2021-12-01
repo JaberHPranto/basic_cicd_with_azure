6b71f9fa538022c59a6a1b349932272b77052f78
JaberHPranto <jabearhossain@iut-dhaka.edu>
2021-12-01 11:54:42 +0600

    updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get("/",(req,res)=>{
-    res.send("hello world")
+    res.send("Basic Ci/Cd with azure")
 })
 
 app.listen(5000,()=>{
